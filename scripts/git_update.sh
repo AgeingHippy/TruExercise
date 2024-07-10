86ae980521b2171e023f29f2a3c2bd1c8b52a972
AgeingHippy <charles_mcdonald@hotmail.com>
2024-07-10 13:39:12 +0100

    Update tagging to return current version if tag already exists on the current commit

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -57,7 +57,8 @@ if [ -z "$NEEDS_TAG" ]; then
   git push --tags
   git push
 else
-  echo "Already a tag on this commit"
+  echo "Already a tag on this commit. Returning existing tag. " +$CURRENT_VERSION
+  NEW_TAG=$CURRENT_VERSION
 fi
 
 echo ::set-output name=git-tag::$NEW_TAG
